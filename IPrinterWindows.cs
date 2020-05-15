60fada26675df8e9e5eccae9f8826307110f5b7a
brigadarmawan <brigadarmawan@gmail.com>
2020-05-15 10:30:39 +0700

    Inisialisasi project

---
+++ IPrinterWindows.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanPolymorphism2
+{
+    public interface IPrinterWindows
+    {
+        void Show();
+        /*{
+            Console.WriteLine("\nPrinter display dimension");
+        }*/
+        void Print();
+        /*{
+            Console.WriteLine("\nPrinter printing");
+        }*/
+    }
+}
